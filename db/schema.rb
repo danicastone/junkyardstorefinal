6e01ed697d768980fe7ef4dabd87a306374ebf86
Danica Nuccitelli <danicastone@gmail.com>
2015-04-24 03:46:40 +0000

    Added description element to database successfully

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319011152) do
+ActiveRecord::Schema.define(version: 20150424032356) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -46,10 +46,11 @@ ActiveRecord::Schema.define(version: 20150319011152) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.decimal  "price",      precision: 12, scale: 3
+    t.decimal  "price",       precision: 12, scale: 3
     t.boolean  "active"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|

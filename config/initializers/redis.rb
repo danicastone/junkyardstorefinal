1d22681f043591467c6496eeb820e12c6f6c3a20
Danica Nuccitelli <danicastone@gmail.com>
2015-04-29 01:03:54 +0000

    fiddling more

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-$redis = Redis.new(:driver => :hiredis) 
-uri = URI.parse(ENV["redis://redistogo:b85e09a83b93927afc85322a6bbd4540@grouper.redistogo.com:10744/"])
+# $redis = Redis.new(:driver => :hiredis) 
+#uri = URI.parse(ENV["redis://redistogo:b85e09a83b93927afc85322a6bbd4540@grouper.redistogo.com:10744/"])
 #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
+uri = URI.parse(ENV["REDISTOGO_URL"]) 
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)

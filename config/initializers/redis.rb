26fa2850e2e0fe6ea3742b03c195153f4cd4a918
Danica Nuccitelli <danicastone@gmail.com>
2015-04-29 19:08:29 +0000

    tweak to test heroku branch

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 #$redis = Redis.new(:driver => :hiredis) 
-#uri = URI.parse(ENV["redis://redistogo:b85e09a83b93927afc85322a6bbd4540@grouper.redistogo.com:10744/"])
+uri = URI.parse(ENV["redis://redistogo:b85e09a83b93927afc85322a6bbd4540@grouper.redistogo.com:10744/"] ||= 'redis://localhost:6379')
+
 #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-uri = URI.parse(ENV["REDISTOGO_URL"]) 
+#uri = URI.parse(ENV["REDISTOGO_URL"]) 
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :payments
 
-g  get 'order_items/create'
+  get 'order_items/create'
 
   get 'order_items/update'
 

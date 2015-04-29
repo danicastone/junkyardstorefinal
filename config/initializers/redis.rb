7598aa9a1dbe423d66daf5a4fb6f7a18fd5e8843
Danica Nuccitelli <danicastone@gmail.com>
2015-04-29 00:22:21 +0000

    removing redis=localhost from development.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 #$redis = Redis.new(:driver => :hiredis) 
 uri = URI.parse(ENV["redis://redistogo:b85e09a83b93927afc85322a6bbd4540@grouper.redistogo.com:10744/"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
+

03c9df93cfbcb1a250e99192b07a1307679688ff
Danica Nuccitelli <danicastone@gmail.com>
2015-05-11 16:59:47 +0000

    Fixed the string of nomethod errors!

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,16 @@
+#Rails.configuration.stripe do |config|
+#  config.publishable.key = ENV['STRIPE_PUBLISHABLE_KEY'] ||= Rails.application.secrets.stripe_publishable_key
+#  config.secret.key = ENV['STRIPE_SECRET_KEY'] ||= Rails.application.secrets.stripe_secret_key
+#end
+#
+#DO NOT USE: 
+#Stripe.api_key = Rails.configuration.stripe.secret_key
+
 Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||= Rails.application.secrets.stripe_publishable_key, 
   :secret_key => ENV['STRIPE_SECRET_KEY'] ||= Rails.application.secrets.stripe_secret_key,
 }
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+#
+#DO NOT USE:
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]

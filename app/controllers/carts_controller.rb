9bc52ff32b781eb21ba56bee1b561c18092a314e
Danica Nuccitelli <danicastone@gmail.com>
2015-05-11 20:40:31 +0000

    updating gemfile to make stripe sources match

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
-  before_action :authenticate_user!
+#require 'redis'
 
+  before_action :authenticate_user!
 
   def show
     cart_ids = $redis.smembers current_user_cart

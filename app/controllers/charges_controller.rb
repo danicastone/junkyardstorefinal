0a72ff0d334ab17c6629cb364bac41ad3a716493
Danica Nuccitelli <danicastone@gmail.com>
2015-05-13 21:41:35 +0000

    Checkout button added, display errors resolved

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,30 @@
 class ChargesController < ApplicationController
-
-  def new 
-   end 
-
+  def new
+    # this will remain empty unless you need to set some instance variables to pass on
+  end
+ 
   def create
     # Amount in cents
-    @amount = 500
+    amount = params[:stripeAmount].to_i * 100
+ 
+    # Create the customer in Stripe
     customer = Stripe::Customer.create(
-      :email => 'example@stripe.com', 
-      :card => params[:stripeToken]
+      email: params[:stripeEmail],
+      card: params[:stripeToken]
     )
-  charge = Stripe::Charge.create(
-    :customer => customer.id, 
-    :amount => @amount, 
-    :description => 'Rails Stripe customer', 
-    :currency => 'usd'
-  ) 
-
+ 
+    # Create the charge using the customer data returned by Stripe API
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
+    )
+ 
+    # place more code upon successfully creating the charge
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to charges_path 
- end
+    redirect_to charges_path
+    flash[:notice] = "Please try again"
+  end
+end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,10 +7,11 @@
 #Stripe.api_key = Rails.configuration.stripe.secret_key
 
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'] ||= Rails.application.secrets.stripe_publishable_key, 
-  :secret_key => ENV['STRIPE_SECRET_KEY'] ||= Rails.application.secrets.stripe_secret_key,
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key => ENV['STRIPE_SECRET_KEY'],
 }
 
 #
 #DO NOT USE:
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+#Rails.configuration.stripe[:secret_key]

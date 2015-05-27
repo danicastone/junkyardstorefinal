3f8a95af6e8448548cfad958ce51073471de3ef5
Danica Nuccitelli <danicastone@gmail.com>
2015-05-27 22:36:25 +0000

    added <%= f.file_field :photo %> to form partial, <%= image_tag @product.photo.url(:small) %> to app/views/products/index, app/views/carts/show,

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,12 @@ class Product < ActiveRecord::Base
 
   validates_numericality_of :price,
     greater_than: 49,
-    message: "The smallest transaction amount Stripe allows is 50 cents, so we've limited the cost of one item to be at least 50 cents."
+    message: "The smallest transaction amount Stripe allows is 50 cents, so we've limited the cost of one item to be at least 50 cents." 
+
+  has_attached_file :photo, :styles => { :small => "150x150>" },
+                  :url => "/assets/products/:id/:style/:basename.:extension", path => 
+                  :":rails_root/public/assets/products/:id/:style/:basename.:extension"
+  validates_attachment_presence :photo validates_attachment_size :photo, :less_than => 50.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
 end

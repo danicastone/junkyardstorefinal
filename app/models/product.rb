87709663882e5af965f40ca66cdd626f56825760
Danica Nuccitelli <danicastone@gmail.com>
2015-06-04 21:05:16 +0000

    polished layout, added logo header, and fixed db rollbacks

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,14 +4,12 @@ class Product < ActiveRecord::Base
   default_scope { where(active: true) }
 
   validates_numericality_of :price,
-    greater_than: 49,
+    greater_than: 0.49,
     message: "The smallest transaction amount Stripe allows is 50 cents, so we've limited the cost of one item to be at least 50 cents." 
 
   has_attached_file :photo, :styles => { :small => "150x150>" },
                   :url => "/assets/products/:id/:style/:basename.:extension", 
                   :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
-  validates_attachment_presence :photo 
-  validates_attachment_size :photo, :less_than => 50.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Product.delete_all 
-Product.create! id: 1, name: "Banana", price: 0.49, active: true 
-Product.create! id: 2, name: "Apple", price: 0.29, active: true 
+Product.create! id: 1, name: "Banana", price: 1.99, active: true 
+Product.create! id: 2, name: "Apple", price: 2.99, active: true 
 Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true 
+Product.create! id: 4, name: "Feet", price: 199.99, active: true
+Product.create! id: 5, name: "Batteries", price: 456.00, active: true
 
 OrderStatus.delete_all 
 OrderStatus.create! id: 1, name: "In Progress" 
